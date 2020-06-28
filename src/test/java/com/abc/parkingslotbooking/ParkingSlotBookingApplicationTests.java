7160472ec871885261bc0d53b60388b38501628d
grandhe10 <sumagrandhe10@gmail.com>
2020-06-28 15:26:59 +0530

    changes

---
+++ src/test/java/com/abc/parkingslotbooking/ParkingSlotBookingApplicationTests.java
@@ -0,0 +1,29 @@
+package com.abc.parkingslotbooking;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.abc.parkingslotbooking.controller.EmployeeController;
+import com.abc.parkingslotbooking.controller.RequestsController;
+
+@SpringBootTest
+class ParkingSlotBookingApplicationTests {
+
+	@Autowired
+	RequestsController requestsController;
+	@Autowired
+	EmployeeController employeeController;
+	@Test
+	public void contexLoads() throws Exception {
+		assertThat(requestsController).isNotNull();
+	}
+
+	@Test
+	public void contexLoads1() throws Exception {
+		assertThat(employeeController).isNotNull();
+	}
+
+}
